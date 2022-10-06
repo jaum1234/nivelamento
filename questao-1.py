e0acccbdd431783fa027e6dd54b74b7b3dfdb799
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-10-06 09:44:26 -0300

    questao 1 finalizada

--- questao-1.py
+++ questao-1.py
@@ -1,9 +1,13 @@
-numbers = [];
-
-numbers.append()
+numbers = [1, 7, 2, 9];
 
 def sequence(numbers):
     if(len(numbers) == 100):
         return numbers;
     
+    numbers.append(numbers[len(numbers) - 3] + numbers[len(numbers) - 4]);
+    
     sequence(numbers);
+    
+sequence(numbers);
+
+print(numbers);
