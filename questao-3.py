ab243689483d60e179f379adc618486ca3e1be63
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-10-08 16:09:32 -0300

    corrindo erro de ortografia na questao 3

--- questao-3.py
+++ questao-3.py
@@ -1,7 +1,7 @@
-# Respsota: Para realizarmos esta operação, inserimos a palavra numa pilha, uma letra em cada espaço e depois, retiramos todas as letras da pilha. Na pilha, os elementos entram pela lógica de "O primeiro elemento que entra, é o último a sair". 
+# Resposta: Para realizarmos esta operação, inserimos a palavra numa pilha, uma letra em cada espaço, e depois retiramos todas as letras da pilha. Nessa estrutura de dados, os elementos seguem a lógica "First in, last out". 
 
-#Por exemplo: Ao inserirmos a palavra "BALÃO", após retirar cada caracter, os caracteres estarão da seguinte forma:
-#O Ã L A B
+# Exemplo: Ao inserirmos a palavra "BALÃO", após retirar cada caracter, a palavra estará na seguinte forma:
+# O Ã L A B
 
-#p.s.: exercicio 6 lista 1 (EDD1)
+# p.s.: exercicio 6 lista 1 (EDD1)
 
