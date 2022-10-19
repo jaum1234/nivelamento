3be7074f6b4a145cc423f4fcd25bea5f2c126a13
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-10-19 13:15:22 -0300

    corrigindo questao 3

--- questao-3.py
+++ questao-3.py
@@ -1,3 +1,5 @@
+from data_structures.stack import Stack;
+
 # Resposta: Para realizarmos esta operação, inserimos a palavra numa pilha, uma letra em cada espaço, e depois retiramos todas as letras da pilha. Nessa estrutura de dados, os elementos seguem a lógica "First in, last out". 
 
 # Exemplo: Ao inserirmos a palavra "BALÃO", após retirar cada caracter, a palavra estará na seguinte forma:
@@ -5,3 +7,17 @@
 
 # p.s.: exercicio 6 lista 1 (EDD1)
 
+stack = Stack();
+
+sentence = "Hello, world!";
+
+for letter in sentence:
+    stack.push(letter);
+    
+reversedSentence = "";
+
+while (not stack.empty()):
+    reversedSentence += stack.pop()
+
+
+print(reversedSentence);
